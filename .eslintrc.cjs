e36f1c5cc19845c74876891b00854457447e80f0
czhlin <2324133088@qq.com>
2023-07-22 02:45:50 +0800

    feat: 添加stylelit、husky

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,42 +1,31 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:vue/vue3-essential"
-    ],
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "parser": "@typescript-eslint/parser",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "@typescript-eslint",
-        "vue"
-    ],
-    "rules": {
-        'no-console': process.env.NODE_ENV === 'production' ? ['error', { allow: ['error', 'warn'] }] : 'off', //生产模式不允许使用log
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:vue/vue3-essential'],
+	overrides: [
+		{
+			env: {
+				node: true,
+			},
+			files: ['.eslintrc.{js,cjs}'],
+			parserOptions: {
+				sourceType: 'script',
+			},
+		},
+	],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		parser: '@typescript-eslint/parser',
+		sourceType: 'module',
+	},
+	plugins: ['@typescript-eslint', 'vue'],
+	rules: {
+		'no-console': process.env.NODE_ENV === 'production' ? ['error', { allow: ['error', 'warn'] }] : 'off', //生产模式不允许使用log
 		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off', //生产默认不允许使用debugger
-		'@typescript-eslint/no-unused-vars': [
-            'error',
-             { varsIgnorePattern: '.*', args: 'none' }
-        ], //变量声明未使用
+		'@typescript-eslint/no-unused-vars': ['error', { varsIgnorePattern: '.*', args: 'none' }], //变量声明未使用
 		'@typescript-eslint/no-explicit-any': 'off', // 允许ts使用any
-    }
-}
+		'linebreak-style': 'error',
+	},
+};
--- types/vue.d.ts
+++ types/vue.d.ts
@@ -1,6 +1,6 @@
 import * as Vue from 'vue';
 //自定义全局变量
-declare module '@vue/runtime-core' {
+declare module 'vue' {
 	import type { ElConfigProvider } from 'element-plus';
 	export interface ComponentCustomProperties {
 		$ELEMENT: ElConfigProvider;
